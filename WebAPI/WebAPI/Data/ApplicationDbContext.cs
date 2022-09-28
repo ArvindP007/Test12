48e2a2f384f2b0b710106836a33cebf2d7d100c8
arvindd <patkalarvind@gmail.com>
2022-09-28 16:51:39 +0530

    End of Section 2

---
+++ WebAPI/WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models.Domain;
+
+namespace WebAPI.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
---
+++ WebAPI/WebAPI/Models/Domain/Region.cs
@@ -0,0 +1,18 @@
+﻿namespace WebAPI.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string RegionName { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long population { get; set; }
+
+        //Navigation Properties
+        public IEnumerable<Walk> Walks { get; set; }
+        
+
+    }
+}
