8848017dadb8e5814d8c406d8759dd7c38bba23e
arvindd <patkalarvind@gmail.com>
2022-11-17 17:02:08 +0530

    Unit_Test_Mock1_Add

---
+++ UnitTest_Mock1/Services/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnitTest_Mock1.Model;
+namespace UnitTest_Mock1.Services
+{
+    public interface IEmployeeService
+    {
+        Task<string> GetEmployeebyId(int EmpID);
+        Task<Employee> GetEmployeeDetails(int EmpID);
+    }
+}
